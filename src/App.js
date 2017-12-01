1813e007042675f25df180254688d0e7a0e30a72
AdithyaKrishnaK <vineetha.kadithya2001@gmail.com>
2017-12-01 07:59:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App-title" id="t"></h1>
+        <h1 id ="blink" > : </h1>
+        <h1 className="App-title" id="m"></h1>
+      </div>
+      
+    );
+
+  }
+}
+window.setInterval(now,2000);
+function now(){
+var d =new Date();
+document.getElementById("t").innerHTML = d.getHours();
+document.getElementById("m").innerHTML = d.getMinutes();}
+export default App;
